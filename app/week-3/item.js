98406c4c1aaa74b0fe15ee5a142fb1c1250b03f0
StevenBrar <stevenbrar1@gmail.com>
2025-09-26 22:05:53 -0600

    I lied, now finished

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,12 +1,12 @@
 
 export default function Item({ name, quantity, category }) {
     return (
-      <li className="border-b py-2 flex justify-between items-center">
+      <li className="border-b flex justify-between items-center">
         <div>
           <span className="font-bold text-blue-800">{name}</span>
-          <span className="text-sm text-black-500 ml-2">({category})</span>
+          <span className="text-sm text-black-500 ">({category})</span>
         </div>
-        <span className="text-sm text-black-600">Qty: {quantity}</span>
+        <span className="text-lg text-black-600">Qty: {quantity}</span>
       </li>
     );
   }
