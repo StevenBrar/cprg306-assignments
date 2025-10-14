f0b77d59227e544b3333b0e543bc7b87e0d202dc
StevenBrar <stevenbrar1@gmail.com>
2025-10-14 15:46:32 -0600

    Creation of week 5

---
+++ app/week-5/page.js
@@ -0,0 +1,9 @@
+import NewItem from './new-item';
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-black-60 flex items-center justify-center p-6">
+      <NewItem />
+    </main>
+  );
+}
